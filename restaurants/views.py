d468948a478694b104d3123d58a63ae244691a7e
atalotaibi <alotaibiatallah@gmail.com>
2019-02-10 16:58:49 +0300

    task_03

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+        "my_list": [
+            {
+                "restaurant_name" : 'McDonalds',
+                "food_type": 'Fast Food'
+            },
+            {
+                "restaurant_name" : 'Tarib',
+                "food_type": 'Traditional'
+            },
+            {
+                "restaurant_name" : 'Red Lobstor',
+                "food_type": 'Sea Food'
+            }
+        ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +27,13 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+        "my_object" : {
+            "restaurant_name" : 'Tarib',
+            "food_type": 'Traditional',
+            "created_on": 'Yesterday',
+            "last_updated_on": 'Today',
+            "author": 'Me and Me'
+        }
 
     }
     return render(request, 'detail.html', context)
